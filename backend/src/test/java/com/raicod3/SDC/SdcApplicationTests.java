5204dfb9dadde36dc81427e4e6ad3847cf3e00dc
Bikal Rai <bikalrai2785@gmail.com>
2025-09-02 21:35:41 +0545

    first commit

---
+++ backend/src/test/java/com/raicod3/SDC/SdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raicod3.SDC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
