b2877051376ece1f0991f96352a5fa9ca2f20293
Bikal Rai <bikalrai2785@gmail.com>
2025-09-03 21:03:28 +0545

    add getters and setters in user model and add constructor

--- backend/src/main/java/com/raicod3/SDC/models/UserModel.java
+++ backend/src/main/java/com/raicod3/SDC/models/UserModel.java
@@ -16,4 +16,87 @@ public class UserModel {
 
     @OneToOne(mappedBy = "user")
     private KYCModel userKyc;
+
+    private String role;
+
+    public UserModel() {
+    }
+
+    public UserModel(int id, String fullName, String email, String phone, String password, KYCModel userKyc, String role) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.userKyc = userKyc;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public KYCModel getUserKyc() {
+        return userKyc;
+    }
+
+    public void setUserKyc(KYCModel userKyc) {
+        this.userKyc = userKyc;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", password='" + password + '\'' +
+                ", userKyc=" + userKyc +
+                '}';
+    }
 }
