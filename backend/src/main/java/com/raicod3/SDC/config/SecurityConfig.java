fdbff46ab66d842ed41ca6f9ba43778bee507318
Bikal Rai <bikalrai2785@gmail.com>
2025-09-06 16:49:11 +0545

    config security filterchain and add user repo and add custom user details

---
+++ backend/src/main/java/com/raicod3/SDC/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.raicod3.SDC.config;
+
+import com.raicod3.SDC.custom.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/", "/login", "/logout").permitAll());
+
+        return http.build();
+    }
+}
---
+++ backend/src/main/java/com/raicod3/SDC/custom/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.raicod3.SDC.custom;
+
+import com.raicod3.SDC.models.UserModel;
+import com.raicod3.SDC.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<UserModel> userOp = userRepository.findByEmail(username);
+
+        if(!userOp.isPresent()) {
+            userOp = userRepository.findByPhone(username);
+        }
+
+        UserModel user = userOp.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user);
+    }
+}
