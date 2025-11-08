5978b470993608d6be15c817a2baa1f7a8689312
Bikal Rai <bikalrai2785@gmail.com>
2025-11-08 11:37:54 +0545

    rework start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './pages/login'
-import Register from './pages/register'
-import Index from './pages/index'
-import Profile from './pages/Profile'
-import CompleteProfile from './pages/completeProfile'
-import Verification from './pages/verificationPage'
-import LandingPage from './pages/landingPage'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Index from "./pages/Index";
+import Profile from "./pages/Profile";
+import CompleteProfile from "./pages/CompleteProfile";
+import Verification from "./pages/VerificationPage";
+import LandingPage from "./pages/LandingPage";
 
 const App = () => {
   return (
     <Router>
-          <Routes>
-            {/* Public routes */}
-            <Route path="/" element={<Index />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/complete-profile" element={<CompleteProfile />} />
-            <Route path="/verification" element={<Verification />} />
-            <Route path="/landing" element={<LandingPage />} />
+      <Routes>
+        {/* Public routes */}
+        <Route path='/' element={<Index />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/complete-profile' element={<CompleteProfile />} />
+        <Route path='/verification' element={<Verification />} />
+        <Route path='/landing' element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
-          </Routes>
-        </Router>
-  )
-}
-
-export default App
+export default App;
