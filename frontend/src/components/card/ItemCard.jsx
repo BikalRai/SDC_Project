6d8f98d9fedce461a1098f146a419e2368899554
Bikal Rai <bikalrai2785@gmail.com>
2025-11-11 20:08:35 +0545

    remove icon import

--- frontend/src/components/card/ItemCard.jsx
+++ frontend/src/components/card/ItemCard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { MdShoppingCart } from "react-icons/md";
 import { FaChevronRight } from "react-icons/fa6";
 import PrimaryButton from "../buttons/PrimaryButton";
 
